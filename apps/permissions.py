46408ac25141f80f30980aa795136e90f3acb27d
Kholmumin <xolmomin@gmail.com>
2025-11-08 10:02:31 +0500

    add tasks

---
+++ apps/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'GET':
+            return True
+        else:
+            return request.user.is_superuser
+
+
+class IsRegisteredUser(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_active and request.user.is_registered
